9a94b052d46c88f805702066a122a1e8d8b6466b
Vlad Tymoshko <slradbez@gmail.com>
2025-02-05 21:22:15 +0100

    migrations final

--- cinema/urls.py
+++ cinema/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemahall/", cinemahall_list, name="cinema-hall-list"),
-    path("cinemahall/<int:pk>/", cinemahall_detail, name="cinema-hall-detail"),
+    path("cinema_halls/", cinemahall_list, name="cinema-hall-list"),
+    path("cinema_halls/<int:pk>/",
+         cinemahall_detail, name="cinema-hall-detail"),
 ]
